cc77f88aa95dc5746a304765236a7b821cc67d39
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-10 02:09:47 +0530

    Final Commit (Traffic Light)

--- Project 4 - Traffic Lights/src/App.jsx
+++ Project 4 - Traffic Lights/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const red1 = useRef();
+  const yellow1 = useRef();
+  const green1 = useRef();
+
+
+  useEffect(() => {
+
+    setRed();
+    setYellow();
+    setGreen();
+
+  }, [])
+
+  const setRed = () => {
+    setInterval(() => {
+      red1.current.classList.toggle('red')
+    }, 4000);
+  }
+
+  const setYellow = () => {
+    setInterval(() => {
+      yellow1.current.classList.toggle('yellow')
+    }, 500);
+  }
+
+  const setGreen = () => {
+    setInterval(() => {
+      green1.current.classList.toggle('green')
+    }, 3000);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="lights">
+
+        <div ref={red1} className="light red"></div>
+
+        <div ref={yellow1} className="light yellow"></div>
+
+        <div ref={green1} className="light green"></div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
