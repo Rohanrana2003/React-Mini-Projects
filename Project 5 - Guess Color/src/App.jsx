6ccc0f57fca3af678806f4c2d9cfd6f5ca9cecf3
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-20 22:46:58 +0530

    Optimised Solution - Guess Color

--- Project 5 - Guess Color/src/App.jsx
+++ Project 5 - Guess Color/src/App.jsx
@@ -1,53 +1,54 @@
-import { useState, useEffect ,useRef } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react"
 
 function App() {
-  const colorArray = [
-    ['#9C334E', '#5607EE', '#C7DF67'],
-    ['#FDFC03', '#A866FD', '#16D505'],
-    ['#C90F57', '#C2F78C', '#75A2AB']
-  ]
-  
-  const [colorIndex, setColorIndex] = useState(0);
-  const [colors, setColors] = useState(colorArray[colorIndex])
-  const [result, setResult] = useState(null)
 
-  let randomColor = colors[Math.floor(Math.random()*3)];
+  const [color, setColor] = useState(null);
+  const [result, setResult] = useState(null);
+  const [allColors, setAllColors] = useState(null);
+
+  function getRandomColor() {
+    for (let i = 0; i < 10; i++) {
+      const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
+      return randomColor;
+    }
+  }
+
+  const pickColor = () =>{
+    let actualColor = getRandomColor();
+    setColor(actualColor);
+    setAllColors([actualColor, getRandomColor(), getRandomColor()].sort(()=>0.5 - Math.random()))
+  }
 
   useEffect(() => {
-    setColors(colorArray[colorIndex]);
-  }, [colorIndex])
-  
+    pickColor();
+  }, [])
 
-  const handleClick = (e)=>{
-    if(e.target.innerText == randomColor){
-      console.log(true);
-      setResult('Correct')
+  function handleClick(e) {
+    if (e.target.innerText === color){
+      setResult('Correct');
+      pickColor();
     }
-    else if(e.target.innerText != randomColor){
-      console.log(false);
-      setResult('Incorrect')
-    }
-
-    if(colorIndex < colorArray.length-1){
-      setColorIndex(colorIndex + 1)
-    }
+    else{
+      setResult('Incorrect');
+    }  
   }
 
   return (
     <div className="w-screen h-screen ">
 
-      <div style={{background:randomColor}} className={`absolute top-[40%] left-[50%] -translate-x-[50%] -translate-y-[50%]  w-[30vw] h-[30vh]`}></div>
+      <div style={{ background: color }} className={`absolute top-[40%] left-[50%] -translate-x-[50%] -translate-y-[50%]  w-[30vw] h-[30vh]`}></div>
 
       <div className=" flex justify-center w-screen items-center flex-row gap-10 relative top-[60%]">
         {
-          colors.map((color, i) => (
-            <p key={i} onClick={handleClick}
+          allColors && allColors.map((color, i) => (
+            <p onClick={handleClick} key={i}
               className="bg-slate-500 text-white px-6 py-2 border-1 border-black border cursor-pointer">{color}</p>
           ))
         }
       </div>
 
-      {result && <p className="font-bold text-3xl">{result}</p>}
+      {result && <p className={`font-bold w-screen text-center text-3xl text-${result == 'Correct' ? 'green-400' : 'red-500'}`} >{result}</p>}
 
     </div>
   )
