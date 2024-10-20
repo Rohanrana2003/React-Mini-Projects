1cfb8d47c32e46db1716e643ba3b5a34b87e5a9f
Rohanrana2003 <rohanrana33042@gmail.com>
2024-10-21 00:18:13 +0530

    Final Commit - Undo Redo

--- Project 6 - Undo Redo/src/App.jsx
+++ Project 6 - Undo Redo/src/App.jsx
@@ -5,36 +5,51 @@ import { useState } from "react";
 function App() {
 
   const [points, setPoints] = useState([])
+  const [popped, setPopped] = useState([]);
 
-const handleClick = (e) =>{
-  const {clientX, clientY} = e;
-  setPoints([...points, {x:clientX, y:clientY}]);
+  const handleClick = (e) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, { x: clientX, y: clientY }]);
+  }
+  
+  const handleUndo = () => {
+    let newArr = [...points];
+    const poppedPoint = newArr.pop();
+    if (!poppedPoint) return;
+    setPopped([...popped, poppedPoint]);
+    setPoints(newArr);
+  }
 
-}
-const handleUndo = () =>{
-  console.log(points)
-  setPoints([...points, points.pop()]);
-}
+  const handleRedo = () => {
+    let newPopped = [...popped];
+    const poppedPoint = newPopped.pop();
+    if (!poppedPoint) return;
+    setPoints([...points, poppedPoint]);
+    setPopped(newPopped);
+  }
 
   return (
-  <>
-    <button onClick={handleUndo}
-      className="bg-slate-400 text-white rounded-lg px-2 py-1 border-black m-[30px]">Undo</button>
+    <div className="bg-slate-900">
+      <button onClick={handleUndo} disabled={points.length === 0}
+        className="bg-slate-400 text-white rounded-lg px-2 py-1 border-black m-[30px]">Undo</button>
 
-    <div onClick={handleClick} className="container min-w-full h-[90vh] bg-slate-800">
+      <button onClick={handleRedo} disabled={popped.length === 0}
+        className="bg-slate-400 text-white rounded-lg px-2 py-1 border-black m-[30px]">Redo</button>
 
-      {
-        points && points.map((point, i)=>(
-          <div key={i} className="circle" 
-          style={{
-            top:point.y + 'px',
-            left:point.x + 'px',
-          }}>
-          </div>
-        ))
-      }
+      <div onClick={handleClick} className="container min-w-full h-[90vh]">
+
+        {
+          points && points.map((point, i) => (
+            <div key={i} className="circle"
+              style={{
+                top: point.y + 'px',
+                left: point.x + 'px',
+              }}>
+            </div>
+          ))
+        }
+      </div>
     </div>
-  </>
 
   )
 }
